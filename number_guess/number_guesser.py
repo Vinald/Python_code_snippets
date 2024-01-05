b8bc4114e79507d67d51158338953814ee17df6a
Vinald <okiror1vinald@gmail.com>
2024-01-05 19:30:40 +0300

    project -> number guessing

--- number_guess/number_guesser.py
+++ number_guess/number_guesser.py
@@ -1,39 +1,52 @@
 import random
 
-print('Welcome to the number guessing game\n')
-top_number = input('Enter a maximum range to guess from: \n')
 
-if top_number.isdigit():
-    top_number = int(top_number)
+def get_top_number():
+    top_number = input('Enter a maximum range to guess from: \n')
 
-    if top_number <= 0:
-        print('Please enter a number greater than zero')
-        quit()
-else:
-    print('Please enter a number next time\n')
-    quit()
+    if top_number.isdigit():
+        top_number = int(top_number)
 
-
-random_number = random.randint(0, top_number)
-guesses = 0
-
-while True:
-    guesses += 1
-
-    user_guess = input(f'Make a guess of a random number between 0 and {top_number}\n')
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
+        if top_number <= 0:
+            print('Please enter a number greater than zero')
+            quit()
     else:
         print('Please enter a number next time\n')
-        continue
+        quit()
 
-    if user_guess == random_number:
-        print('Correct guess')
-        break
-    elif user_guess > random_number:
-        print('Invalid guess, above the random number!\n')
-    else:
-        print('Invalid guess, below the random number!\n')
+    return top_number
 
 
-print(f'You got it in {guesses} guesses')
+def get_user_guess(top_number):
+    while True:
+        user_guess = input(f'Make a guess of a random number between 0 and {top_number}\n')
+        if user_guess.isdigit():
+            user_guess = int(user_guess)
+            return user_guess
+        else:
+            print('Please enter a number next time\n')
+
+
+def play_game():
+    top_number = get_top_number()
+    random_number = random.randint(0, top_number)
+    guesses = 0
+
+    while True:
+        guesses += 1
+
+        user_guess = get_user_guess(top_number)
+
+        if user_guess == random_number:
+            print('Correct guess')
+            break
+        elif user_guess > random_number:
+            print('Invalid guess, above the random number!\n')
+        else:
+            print('Invalid guess, below the random number!\n')
+
+    print(f'You got it in {guesses} guesses')
+
+
+print('Welcome to the number guessing game\n')
+play_game()
---
+++ rock_paper_scissors/main.py
@@ -0,0 +1,31 @@
+import random
+
+
+def play_game():
+    choices = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(choices)
+    user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+
+    print(f"Computer chose: {computer_choice}")
+    print(f"You chose: {user_choice}")
+
+    if user_choice not in choices:
+        print("Invalid choice. Please try again.")
+        return
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'paper' and computer_choice == 'rock') or \
+         (user_choice == 'scissors' and computer_choice == 'paper'):
+        print("You win!")
+    else:
+        print("Computer wins!")
+
+
+while True:
+    play_game()
+
+    play_again = input('Play Again? (yes/no)').lower()
+    if play_again != 'yes':
+        break
