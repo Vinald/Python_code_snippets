f6ea754185c625095f3645890282f024b7d2e57f
Vinald <okiror1vinald@gmail.com>
2024-01-07 16:48:12 +0300

    Password manager

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,17 +1,40 @@
+from cryptography.fernet import Fernet
+
+
+# key
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+
+# write_key()
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+
 master_pwd = input('What is your master password? \n')
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 
 def view():
     with open('password.txt', 'r') as f:
         for line in f.readlines():
-            print(line, end='')
-            
+            data = line.split('|')
+            user, passwd = data[0], data[1]
+            print('User:', user, '| Password:', fer.decrypt(passwd.encode()))
+
+
 def add(**args):
     name = input('Enter your name:\n')
     pwd = input('Enter your password:\n')
 
     with open('password.txt', 'a') as f:
-        f.write('Name: ' + name + '\n' + 'Password: ' + pwd + '\n')
+        f.write(name + '|' + fer.encrypt(pwd.encode()).decode())  # Decode the bytes object to str
 
 
 while True:
